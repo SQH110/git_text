6e009b62365486c9474c5231cda05fd8ac5b47f9
sqh <838574833@qq.com>
2021-05-06 22:28:31 +0800

    first commit

---
+++ src/main/java/com/sqh/HelloGit.java
@@ -0,0 +1,13 @@
+package com.sqh;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author sqh
+ * @create 2021-05-06 22:22
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
